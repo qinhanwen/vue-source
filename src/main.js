83a689ca3b3eaec9db09d05d03f51bf6fab58472
qinhanwen <285074175@qq.com>
2019-09-03 14:02:22 +0800

    依赖收集和派发更新

--- src/main.js
+++ src/main.js
@@ -1,43 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App';
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-Vue.mixin({
-  created() {
-    console.log('parent created')
-  }
-})
-Vue.component('name-box',{
-  template:'<p>{{name}}{{age}}</p>',
-  props:{
-    name:String
-  },
-  data:function(){
-    return {
-      age:25
-    }
-  }
-})
 
 new Vue({
   el: '#app',
   data: function () {
     return {
-      name: 'qinhanwen'
+      message: 'hello',
+      personInfo:{
+        name:'qinhanwen'
+      }
     }
   },
-  components:{
-    App
+  methods: {
+    changeMsg() {
+      this.message = 'Hello World!'
+      this.personInfo.name = 'qhw';
+    }
   },
-  mounted(){
-    console.log('parent mount');
-  },
-  created(){
-    console.log('child created');
-  },
-  template: '<App :name="name"></App>'
+  template: `<div @click="changeMsg">
+  {{ message }}
+  {{personInfo.name}}
+  </div>`
 })
