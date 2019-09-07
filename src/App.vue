410867a96e6c0d36c5de60886ae9eed8cfae2e3d
qinhanwen <285074175@qq.com>
2019-09-07 10:58:23 +0800

    feat:事件绑定

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
- 
+  <button @click="clickHandler($event)">click me</button>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  props:{
-    firstName:String
+  name: "App",
+  props: {
+    firstName: String
   },
-}
+  methods: {
+    clickHandler(e) {
+      console.log("Button clicked!", e);
+      this.$emit("select");
+    }
+  }
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import App from './App';
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -9,14 +10,21 @@ new Vue({
   el: '#app',
   data: function () {
     return {
-      message:"123"
+      message: "123"
     }
   },
-  methods: {
-
+  components: {
+    App
   },
-  template: `<div>
-    <input v-model="message" />
-    {{message}}
+  methods: {
+    clickHandler() {
+      console.log('Child clicked!')
+    },
+    selectHandler() {
+      console.log('Child select!')
+    }
+  },
+  template: `<div> 
+  <App @select="selectHandler" @click.native="clickHandler" />
   </div>`
 })
