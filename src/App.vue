72afec92bd0924627859c396e268fcc9fb8b0ce4
qinhanwen <285074175@qq.com>
2019-09-09 11:16:19 +0800

    feat:绑定多事件

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
-  <div id="app">
-    <h1>Hello App!</h1>
-    <p>
-      <!-- 使用 router-link 组件来导航. -->
-      <!-- 通过传入 `to` 属性指定链接. -->
-      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-      <router-link to="/foo">Go to Foo</router-link>
-      <router-link to="/bar">Go to Bar</router-link>
-    </p>
-    <!-- 路由出口 -->
-    <!-- 路由匹配到的组件将渲染在这里 -->
-    <router-view></router-view>
-  </div>
+  <button @click="clickHandler($event)">click me</button>
 </template>
 
 <script>
-export default {
-  name: "App",
-  props: {
-  },
-
-};
+  export default {
+    name: "App",
+    props: {
+      firstName: String
+    },
+    methods: {
+      clickHandler(e) {
+        console.log('child clicked');
+        this.$emit("childEmitHandler");
+      }
+    }
+  };
 </script>
