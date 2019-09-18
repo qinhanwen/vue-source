94d99a3986ad89832bda95a91d989711d2e0f7d8
qinhanwen <285074175@qq.com>
2019-09-18 12:45:45 +0800

    feat:v-if和v-for使用在同一个模板

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App';
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 
 new Vue({
   el: '#app',
-  data: {
-    name: 'qinhanwen'
+  data: function () {
+    return {
+      arr:[1,2,3]
+    }
   },
-  components: {
-    App
-  },
-  methods: {
 
-  },
-  template: `<App><p slot="content">{{name}}</p></App>`
+  template: `<div>
+    <p v-if="!arr.length" v-for="item in arr">{{item}}</p>
+  </div>`
 })
